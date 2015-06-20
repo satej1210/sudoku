0f233e122343eef4902f08e8c4b4f8739846b33c
mudit2103 <mudit2103@gmail.com>
2015-06-21 00:03:48 +0400

    Added a sample puzzle using a macro

--- Sudoku/main.cpp
+++ Sudoku/main.cpp
@@ -7,7 +7,7 @@
 //
 
 #include <iostream>
-
+#define i(a,b,c) sudoku[a-1][b-1].ChangeState(c)
 using namespace std;
 
 class cell
@@ -20,7 +20,7 @@ class cell
 public:
     
     bool is_filled = false;
-    bool possibilities[9];
+    bool possibilities[9] = {true, true, true, true, true, true, true, true, true};
     bool printed = false;
     
     void ChangeState(int x)
@@ -46,7 +46,7 @@ public:
     
 }sudoku[9][9];
 
-void DrawGrid()
+void DrawGrid()         //only to draw the grid
 {
     cout<<" --- --- --- --- --- --- --- --- ---\n";
     for(int i=0; i<9; i++)
@@ -56,10 +56,6 @@ void DrawGrid()
             for(int j=0; j<9; j++)
             {
                 cout<<"|";
-                /*if (sudoku[j][i].is_filled == true)
-                 cout<<" "<<sudoku[j][i].GetState()<<" ";
-                 else
-                 cout<<"   ";*/
                 if(sudoku[i][j].is_filled && sudoku[i][j].printed==false)
                 {
                     cout<<"\'"<<sudoku[i][j].GetState()<<"\'";
@@ -67,18 +63,18 @@ void DrawGrid()
                 }
                 
                 
-                    if(sudoku[i][j].possibilities[m*3+0]==true&&!sudoku[i][j].is_filled)
-                        cout<<m*3+1;
-                    else if(sudoku[i][j].is_filled==false||m!=0)
-                        cout<<" ";
-                    if(sudoku[i][j].possibilities[m*3+1]==true&&!sudoku[i][j].is_filled)
-                        cout<<m*3+2;
-                    else if(sudoku[i][j].is_filled==false||m!=0)
-                        cout<<" ";
-                    if(sudoku[i][j].possibilities[m*3+2]==true&&!sudoku[i][j].is_filled)
-                        cout<<m*3+3;
-                    else if(sudoku[i][j].is_filled==false||m!=0)
-                        cout<<" ";
+                if(sudoku[i][j].possibilities[m*3+0]==true&&!sudoku[i][j].is_filled)
+                    cout<<m*3+1;
+                else if(sudoku[i][j].is_filled==false||m!=0)
+                    cout<<" ";
+                if(sudoku[i][j].possibilities[m*3+1]==true&&!sudoku[i][j].is_filled)
+                    cout<<m*3+2;
+                else if(sudoku[i][j].is_filled==false||m!=0)
+                    cout<<" ";
+                if(sudoku[i][j].possibilities[m*3+2]==true&&!sudoku[i][j].is_filled)
+                    cout<<m*3+3;
+                else if(sudoku[i][j].is_filled==false||m!=0)
+                    cout<<" ";
                 
             }
             if(m==0)
@@ -89,27 +85,45 @@ void DrawGrid()
         cout<<" --- --- --- --- --- --- --- --- ---\n";
     }
     
+    cout<<"\n\n\n\n\n\n\n\n\n\n\n";
+    
 }
 
 
 
 int main()
 {
-    sudoku[8][8].possibilities[1]=true;
-    sudoku[7][8].possibilities[1]=true;
-    sudoku[0][8].possibilities[1]=true;
-    sudoku[7][8].possibilities[1]=true;
-    sudoku[7][8].possibilities[8]=true;
-    sudoku[0][4].possibilities[1]=true;
-    sudoku[7][8].possibilities[1]=true;
-    sudoku[1][4].possibilities[4]=true;
+    DrawGrid();
     
-    sudoku[7][8].possibilities[1]=true;
-    sudoku[7][8].ChangeState(5);
-    sudoku[3][4].ChangeState(5);
-    sudoku[7][8].possibilities[0]=true;
-    sudoku[7][8].possibilities[2]=true;
     
+    {i(1,2,8);
+        i(1,7,2);
+        i(2,5,8);
+        i(2,6,4);
+        i(2,8,9);
+        i(3,3,6);
+        i(3,4,3);
+        i(3,5,2);
+        i(3,8,1);
+        i(4,2,9);
+        i(4,3,7);
+        i(4,8,8);
+        i(5,1,8);
+        i(5,4,9);
+        i(5,6,3);
+        i(5,9,2);
+        i(6,2,1);
+        i(6,7,9);
+        i(6,8,5);
+        i(7,2,7);
+        i(7,5,4);
+        i(7,6,5);
+        i(7,7,8);
+        i(8,2,3);
+        i(8,4,7);
+        i(8,5,1);
+        i(9,3,8);
+        i(9,8,4);} //creating a sample puzzle
     
     DrawGrid();
 }
