aec13f8c096d675649f2a94c4ddca6ed9577996d
mudit2103 <mudit2103@gmail.com>
2015-06-20 22:08:40 +0400

    Initial Commit

---
+++ Sudoku/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Sudoku
+//
+//  Created by Mudit Gupta on 6/20/15.
+//  Copyright (c) 2015 Mudit Gupta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
